b0508681cb4d624ce881e688eff927e9663ce3fe
setoyama <kotaro.setoyama@gmail.com>
2015-08-02 14:34:09 +0900

    third commit

--- hog.h
+++ hog.h
@@ -3,7 +3,6 @@
 
 #include <vector>
 
-//
 std::vector<float> GetHoG(unsigned char* img, int _SIZE_X, int _SIZE_Y, int _CELL_BIN = 9, int _CELL_X = 5, int _CELL_Y = 5, int _BLOCK_X = 3, int _BLOCK_Y = 3);
 
 #endif
